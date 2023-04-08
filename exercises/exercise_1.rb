7d816264a4853af26868942401282e5b5cb745c4
Pedro Lorandi <hello@pedrolorandi.com>
2023-04-08 03:09:32 -0400

    Finish exercise 02

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,6 +3,7 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+# Your code goes below here ...
 @stores_number = 3
 
 Store.create(name: "Burnaby", annual_revenue: 300_000, mens_apparel: true, womens_apparel: true)
@@ -10,4 +11,3 @@ Store.create(name: "Richmond", annual_revenue: 1_260_000, womens_apparel: true)
 Store.create(name: "Gastown", annual_revenue: 190_000, mens_apparel: true)
 
 puts "Number of stores: #{Store.count}"
-# Your code goes below here ...
